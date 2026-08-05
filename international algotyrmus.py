19e949d3239529e9e4d6443adfee05857a840f17
phantom_ghost454 <dampfhans093@gmail.com>
2026-08-06 00:26:03 +0200

    Add files via upload

---
+++ international algotyrmus.py
@@ -0,0 +1,13 @@
+import time
+
+#🌍 aller welt algorytmus
+print("🤖 ki scannt user aus aller welt")
+time.sleep(10) #wartete 10sekunden
+print("🤖scannt was user aus aller welt gucken")
+time.sleep(5) #wartet 5sekunden
+print("🤖scannt was die meisten gucken")
+time.sleep(3) #wartet 3sekunden
+print("🤖scan fertig")
+time.sleep(1) #wartet 1sekunde
+print("gaming, fortnite, memes, tanzen wird 90prozent der user angezeigt")
+#🌍 aller welt algorytmus
---
+++ kommentaren algorytmus.py
@@ -0,0 +1,57 @@
+import time
+
+#nette kommentare
+print("🤖 ki scannt nette kommentare")
+time.sleep(2) #2sekunden
+print("ki scannt den kommentar, du bist mega")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, du bist stark")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, echt toll was du machst")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, weiter sooo")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Du schaffst das! Ich glaube an dich")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Mag dich!")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Mega sympathisch!")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Du bist meine Sonne.")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Du bist mein Fels in der Brandung.")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar,Du bringst das Beste in mir zum Vorschein.")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Ich habe laut ausgeatmet.")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Du bist so underrated!")
+time.sleep(1) #2sekunden
+
+#hater kommentare
+print("🤖 ki scannt hasserfüllte kommentare")
+time.sleep(2) #2sekunden
+print("ki scannt den kommentar, Das ist doch total schlecht, kommentar gesperrt")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Du hast überhaupt keine Ahnung, kommnetra gesperrt")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Hör lieber auf damit, das bringt nichts, kommentar gesperrt")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Voll cringe, kommentar für manche nicht sichtbar")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Das wird niemals etwas werden, kommentra gesperrt")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Niemand braucht so etwas, kommentar gesperrt")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Du bist einfach nur peinlich, kommentar gesperrt")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Völlig nutzlos, kommentar gesperrt")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Wer schaut sich so einen Müll an?, kommentar gesperrt")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Du machst alles falsch, kommentar für manche nicht sichtbar")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Einfach nur Zeitverschwendung, kommentar für manche nicht sichtbat")
+time.sleep(1) #2sekunden
+print("ki scannt den kommentar, Geh lieber wieder offline, kommentar gesperrt")
+time.sleep(1) #2sekunden
