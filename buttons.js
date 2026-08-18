063e23a2f1fcbd5be53952db060a87337dc5be05
phantom_ghost454 <dampfhans093@gmail.com>
2026-08-18 21:03:39 +0200

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,13 @@
+const ersterButton = document.getElementById('ersterButton');
+
+ersterButton.addEventListener('click', function() {
+  window.open('https://play.google.com/store/games?hl=en', '_blank');
+
+});
+
+const zweiterButton = document.getElementById('zweiterButton');
+
+zweiterButton.addEventListener('click', function() {
+window.open('https://apps.apple.com/de/iphone/apps', '_blank');
+
+});
