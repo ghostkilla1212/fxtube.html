20eb4377b03fb287fa8e0186834249b9649d0276
phantom_ghost454 <dampfhans093@gmail.com>
2026-08-24 22:00:54 +0200

    bug gefixt
    bug wurde in der web app gefixt, die abstände müssten wieder da sein wo sie sind, falls nicht bitte uns melden, danke

--- fxtube.html/buttons.js
+++ fxtube.html/buttons.js
@@ -1,13 +1,6 @@
 const ersterButton = document.getElementById('ersterButton');
 
 ersterButton.addEventListener('click', function() {
-  window.open('http://127.0.0.1:5501/erster%20button%20seite.html', '_blank');
-
-});
-
-const zweiterButton = document.getElementById('zweiterButton');
-
-zweiterButton.addEventListener('click', function() {
-window.open('https://apps.apple.com/de/iphone/apps', '_blank');
+  window.open('https://play.google.com/store/games?hl=en', '_blank');
 
 });
--- fxtube.html/fixtube.js
+++ fxtube.html/fixtube.js
@@ -396,48 +396,6 @@ letzterButton.addEventListener('click', function() {
 //button der dich zu einer anderen seite bringt von fixtube
 
 
-const androidButton = document.getElementById('androidButton');
-
-androidButton.addEventListener('click', function() {
-  window.open('https://play.google.com/store/games?hl=en', '_blank');
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
