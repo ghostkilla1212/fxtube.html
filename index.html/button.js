c1d366120e369cb05f94b9287ebdc235fe4ebb9e
phantom_ghost454 <dampfhans093@gmail.com>
2026-09-07 16:31:40 +0200

    Add files via upload

---
+++ index.html/button.js
@@ -0,0 +1,9 @@
+const fixtubeButton = document.getElementById('hundertButton');
+
+hundertButton.addEventListener('click', function() {
+window.open('http://127.0.0.1:5501/fixtube.html', '_blank');
+
+});
+
+
+
---
+++ index.html/buttons.js
@@ -0,0 +1,9 @@
+const ersterButton = document.getElementById('ersterButton');
+
+ersterButton.addEventListener('click', function() {
+  window.open('https://play.google.com/store/games?hl=en', '_blank');
+
+});
+
+
+
